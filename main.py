6905cda03e9f3e6b955b76905d5f0e03f1fc855c
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-08-01 09:35:28 +0500

    .

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 # print("Hello Rimsha")
 # print(8+8)
-# "Hello"[1:4]
-import time
-timestamp =time.strftime('%H:%M:%S')
-print(timestamp)
-timestamp =time.strftime('%H')
-print(timestamp)
+# # "Hello"[1:4]
+# import time
+# timestamp =time.strftime('%H:%M:%S')
+# print(timestamp)
+# timestamp =time.strftime('%H')
+# print(timestamp)
 
-timestamp =time.strftime('%M')
-print(timestamp)
+# timestamp =time.strftime('%M')
+# print(timestamp)
 
-timestamp =time.strftime('%S')
-print(timestamp)
-import time
-timestamp =time.strftime('Good Morning:Good Afternoon:Good Night:')
-print(timestamp)
-timestamp =time.strftime('Good Morning Sir')
-print(timestamp)
-timestamp =time.strftime('Good Afternoon Sir')
-print(timestamp)
-timestamp =time.strftime('Good Night Sir')
-print(timestamp)
+# timestamp =time.strftime('%S')
+# print(timestamp)
+# import time
+# timestamp =time.strftime('Good Morning:Good Afternoon:Good Night:')
+# print(timestamp)
+# timestamp =time.strftime('Good Morning Sir')
+# print(timestamp)
+# timestamp =time.strftime('Good Afternoon Sir')
+# print(timestamp)
+# timestamp =time.strftime('Good Night Sir')
+# print(timestamp)
 
 
-age =int(input("Enter your age: "))
-if age >= 18:
-  print("You can vote")
-else:
-  print("Sorry you cannot vote")  
+# age =int(input("Enter your age: "))
+# if age >= 18:
+#   print("You can vote")
+# else:
+#   print("Sorry you cannot vote")  
 
-students = ("Rimsha", "Jannat")
-name = students[0]  
-print(name)
+students = ["Rimsha", "Jannat"]
+# name = students[0]  
+# print(name[0:3])
+name = ("orange","Apple")
+students += name
+print(students)
+
+students = ["Rimsha", "Jannat", "Teacher"]  # Initialize as a list
+name = "Alice"
+students.append(name)  # Add name to the list
+print(students)  # ['Alice']
                                              
 
 
